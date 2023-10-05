ce6f159b6b926ab89de4854c9d670957326003e4
Gizelle Lao <84821870+GizelleL@users.noreply.github.com>
2023-10-04 22:24:33 -0400

    Add files via upload

---
+++ CASE_STUDY2_FINALCOPY.sql
@@ -0,0 +1,72 @@
+/* FINAL CASE STUDY 2 CODE */ 
+-- Create donations by month, averagee donation and sum of donations for June
+
+CREATE PROCEDURE DonationsbyDateCity
+AS
+BEGIN
+
+    SELECT 
+        A.city,
+        DATEPART(YEAR, D.donation_date) AS Year,
+        DATEPART(MONTH, D.donation_date) AS Month,
+        AVG(CAST(D.donation_amount AS DECIMAL)) AS avg_donation,
+        SUM(CAST(D.donation_amount AS DECIMAL)) AS sum_donation
+    FROM Donation AS D
+    JOIN Address AS A ON D.addressID = A.addressID
+    GROUP BY 
+        A.city,
+        DATEPART(YEAR, D.donation_date),
+        DATEPART(MONTH, D.donation_date);
+END
+EXEC DonationsbyDateCity;
+
+
+-- Create the stored procedure and name; parameters are city and month, can be selected in execution line
+-- Calculate the average and sum of donations by day, month, and year
+  -- USE DATEPART FUNCTION FOR EASIER READABILITY AND TO EXTRACT COMPONENTS OF DATE COLUMN FOR TABLE
+
+CREATE PROCEDURE totaldonationavgsum (@city NVARCHAR(100), @month INT)
+AS
+BEGIN
+  SELECT 
+    DATEPART(YEAR, d.donation_date) AS Year,
+    DATEPART(MONTH, d.donation_date) AS Month,
+    DATEPART(DAY, d.donation_date) AS Day,
+    AVG(CAST(d.donation_amount AS decimal)) AS avg_donation,
+    SUM(CAST(d.donation_amount AS decimal)) AS sum_donation
+  FROM Donation d
+  INNER JOIN Address a ON d.addressID = a.addressID
+  WHERE 
+    DATEPART(MONTH, d.donation_date) = @month AND
+    a.city = @city
+  GROUP BY 
+    DATEPART(YEAR, d.donation_date),
+    DATEPART(MONTH, d.donation_date),
+    DATEPART(DAY, d.donation_date)
+END
+
+EXEC totaldonationavgsum 'Toronto', 6
+EXEC totaldonationavgsum 'Vancouver', 6
+
+
+ -- Average and sum of donations by postal code and city in a specific month; USING DATEPART FUNCTION FOR MONTH
+CREATE PROCEDURE GetDonationStatisticsByCityAndMonth
+    @city VARCHAR(100),
+    @month INT
+AS
+BEGIN
+
+    SELECT
+        a.city,
+        a.postal_code,
+        AVG(CAST(d.donation_amount AS DECIMAL)) AS avg_donation,
+        SUM(CAST(d.donation_amount AS DECIMAL)) AS sum_donation
+    FROM Donation AS d
+    INNER JOIN Address AS a ON d.addressID = a.addressID
+    WHERE
+        a.city = @city
+        AND DATEPART(MONTH, d.donation_date) = @month
+    GROUP BY a.city, a.postal_code;
+END
+
+EXEC GetDonationStatisticsByCityAndMonth @city = 'Vancouver', @month = 6;
